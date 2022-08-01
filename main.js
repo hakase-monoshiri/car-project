254a3624022ab6b01b6fc854e83990ef456fc35b
Eben Eleazer <ebenaeleazer@outlook.com>
2022-08-01 19:52:49 -0400

    Add road

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+
+
 const canvas = document.getElementById('roadCanvas');
 
 
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-
+const road = new Road(canvas.width/2, canvas.width*.9)
 const car = new Car(100,100,30,50)
 
 car.draw(ctx);
@@ -12,6 +14,7 @@ car.draw(ctx);
 function animate() {
     car.update();
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate)
 }
