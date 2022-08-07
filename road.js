20cc6a02ea0f8872ba4d4fdabb54358bb7f57b70
Eben Eleazer <ebenaeleazer@outlook.com>
2022-08-07 16:44:40 -0400

    Add borders property to road the edges of the reaod have been redefined as borders this will allow them to exhibit unique behaviors compared to other raod lines

--- road.js
+++ road.js
@@ -12,6 +12,16 @@ class Road {
 
         this.top = -almostInfinity;
         this.bottom = almostInfinity;
+
+        const topLeft = {x:this.left, y:this.top};
+        const topRight = {x:this.right, y:this.top};
+        const bottomLeft = {x:this.left, y:this.bottom};
+        const bottomRight = {x:this.right, y:this.bottom};
+
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ]
     }
     
     getLaneCenter(laneIndex) {
@@ -24,29 +34,28 @@ class Road {
         context.lineWidth = 5;
         context.strokeStyle = 'white';
 
-        for( let i = 0; i <= this.laneCount; i++) {
+        for( let i = 1; i <= this.laneCount -1; i++) {
             const x = lerp(
                 this.left,
                 this.right,
                 i/this.laneCount
             );
-
-            if ( i > 0 && i < this.laneCount) {
-                context.setLineDash([]);
-            }
-            else {
-                context.setLineDash([]);
-            }
+               context.setLineDash([20,20]);
 
             context.beginPath();
             context.moveTo(x, this.top);
             context.lineTo(x, this.bottom);
             context.stroke();
-
-
         }
 
-       
+        context.setLineDash([]);
+
+        this.borders.forEach( border => {
+            context.beginPath();
+            context.moveTo(border[0].x, border[0].y);
+            context.lineTo(border[1].x, border[1].y);
+            context.stroke();
+        })
     }
 }
 
