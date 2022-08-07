99383156c63c54b5d737f3abd02290ce6899d8ae
Eben Eleazer <ebenaeleazer@outlook.com>
2022-08-07 16:48:19 -0400

    Introduce "camera" to car view Road context will draw around the car so that the car seems to be stationary

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ car.draw(ctx);
 
 function animate() {
     car.update();
+
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * 0.7 )
+
     road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate)
